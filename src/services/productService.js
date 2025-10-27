a241fcc204cea223c727b14e34d9477de906d407
lelethuml <129957227+lelethuml@users.noreply.github.com>
2025-10-27 04:30:56 +0200

    test

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 // const API_URL = "https://store-payment.onrender.com/api"; //==== hosted on render.com
-       const API_URL ="http://springboot-store-payment-app.eu-north-1.elasticbeanstalk.com/api"; /// hosted on AWS Elastic Beanstalk
+    //    const API_URL ="http://springboot-store-payment-app.eu-north-1.elasticbeanstalk.com/api"; /// hosted on AWS Elastic Beanstalk
 // const API_URL = "http://localhost:8080/api"; //==== local development
+
+         const API_URL ="http://springboot-jpa-app-env.eba-xddamqpi.eu-north-1.elasticbeanstalk.com/api"; /// hosted on AWS Elastic Beanstalk
 
 export const getProduct = (barCode) => axios.get(`${API_URL}/get/products/${barCode}`);
 export const addProduct = (product) => axios.post(`${API_URL}/adding/products`, product);
